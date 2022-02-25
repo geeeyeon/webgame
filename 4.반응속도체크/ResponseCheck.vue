5bf4c70f65e56d86700946095cb3547426ad8a73
geeeyeon <parafunction@hanmail.net>
2022-02-26 01:25:52 +0900

    추가 : 반응속도 체크

---
+++ 4.반응속도체크/ResponseCheck.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>반응속도 체크</h1>
+    <div id="screen" :class="state">{{message}}</div>
+    <div>
+        <div>평균시간: {{}}</div>
+        <button @click="onReset">리셋</button>
+    </div>
+  </div>  
+</template>
+<script>
+export default {
+   data(){ 
+       return{
+           result : [],
+           state: 'waiting',
+           message: '클릭해서 시작하세요'
+        }
+   },
+   methods:{
+       onReset(){
+
+       }
+   }
+};
+</script>
+<style>
+  #screen {
+      width: 300px;
+      height: 200px;
+      text-align: center;
+      user-select: none;
+  }
+  #screen.waiting {
+      background-color:cornflowerblue;
+  }
+  #screen.ready {
+      background-color:lightcoral;
+      color:white;
+  }
+  #screen.now {
+      background-color:lightgreen;
+  }
+</style>
