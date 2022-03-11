a244a5fbffcd1f3e929b54147b2093e79c59cb04
geeeyeon <parafunction@hanmail.net>
2022-03-11 22:42:07 +0900

    추가 : 틱택토

---
+++ 7.틱택토/TicTacToe.vue
@@ -0,0 +1,28 @@
+<template>
+  <table-component :table-data="tableData"/>
+</template>
+<script>
+import TableComponent from './TableComponent';
+export default {
+    components:{ //
+        TableComponent,
+    },
+   data(){ 
+      return{
+          tableData:[
+              ['','',''],
+              ['','',''], 
+              ['','',''],
+          ]
+      }
+   },
+   computed:{ // 일반 데이터를 가공해서 쓸 때(성능상 문제)
+     
+   },
+   methods:{
+   },
+};
+</script>
+<style>
+ 
+</style>
